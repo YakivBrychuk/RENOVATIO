81ca8e35ea4a1a9f9c1ac804cc23ade93b42005c
Yakiv Brychuk <yakiv.brychuk0@gmail.com>
2024-07-03 00:06:55 +0100

    Build Post Model and Connect to Admin Dashboard

--- Renovatio/settings.py
+++ Renovatio/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     ".herokuapp.com",
@@ -95,6 +95,11 @@ WSGI_APPLICATION = 'Renovatio.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
