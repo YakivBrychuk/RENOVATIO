dcb255b95c7763db3ca60ef9f629c0f8a54c6c3f
Yakiv Brychuk <yakiv.brychuk0@gmail.com>
2024-07-04 09:32:35 +0100

    feat: Add Meta class for ordering and __str__ method to Post model
    - Added Meta class to Post model to order posts by created_on in descending order.
    - Implemented __str__ method to display post title and author in a readable format in the admin panel.

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
 # Comment model for posts
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -27,3 +33,7 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
