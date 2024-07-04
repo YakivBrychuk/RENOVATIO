37ccd58ca3cd6cef30094ae6e51d65f23cb7dea6
Yakiv Brychuk <yakiv.brychuk0@gmail.com>
2024-07-04 14:20:35 +0100

    feat: Update URL structure for blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
 from . import views
+from django.urls import path
+
 
 
 # Path: blog/urls.py
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path("", views.PostList.as_view(), name='home'),
 ]
