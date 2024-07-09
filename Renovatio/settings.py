ef0bf23956f731dbd1a722121903dcc25dc8a3f7
Yakiv Brychuk <yakiv.brychuk0@gmail.com>
2024-07-09 20:26:13 +0100

    Properly configure STATIC_ROOT and other settings

--- Renovatio/settings.py
+++ Renovatio/settings.py
@@ -99,9 +99,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+
+# Static files (CSS, JavaScript, Images)
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+# Default primary key field type
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
