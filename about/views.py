166bf7c11ef322d6a1b1b30762f4c53673a030d2
superuser <jaycobneros@protonmail.com>
2024-11-02 18:57:53 +0000

    Update the paths of urls to render about html page

--- about/views.py
+++ about/views.py
@@ -1,41 +1,25 @@
 from django.shortcuts import render
 from django.contrib import messages
 from .models import About
-from .forms import CollaborateForm
+# from .forms import CollaborateForm
 # Create your views here.
 
 
-def about_me(request):
-    """
-    Renders the most recent information on the website author
-    and allows user collaboration requests.
-
-    Displays an individual instance of :model:`about.About`.
-
-    **Context**
-    ``about``
-        The most recent instance of :model:`about.About`.
-        ``collaborate_form``
-            An instance of :form:`about.CollaborateForm`.
-
-    **Template**
-    :template:`about/about.html`
-    """
-
-    if request.method == "POST":
+def about_view(request):
+    if request.method == "POST" and 'collaborate_form' in globals():
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
             collaborate_form.save()
-            messages.add_message(
-                request, messages.SUCCESS,
-                'Collaboration request received! I endeavour to respond within 2 working days.'
+            messages.success(
+                request,
+                'Collaboration request received! I endeavor to respond within 2 working days.'
             )
-    about = About.objects.all().order_by('-updated_on').first()
-    collaborate_form = CollaborateForm()
+    else:
+        collaborate_form = CollaborateForm() if 'CollaborateForm' in globals() else None
 
+    about = About.objects.all().order_by('-updated_on').first()
     return render(
         request,
         "about/about.html",
-        {"about": about,
-         "collaborate_form": collaborate_form},
+        {"about": about, "collaborate_form": collaborate_form},
     )
