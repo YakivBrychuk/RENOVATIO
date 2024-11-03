28c911d6ba2be1657d05f8f024b3849d84b8bb76
superuser <jaycobneros@protonmail.com>
2024-11-03 10:25:09 +0000

    Add comments and comment count to post_detail view in blog

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render, get_object_or_404 
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
-# Create your views here.
-
+# List view for posts
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
 
+# Detail view for a single post
 def post_detail(request, slug):
     """
     Display an individual :model:`blog.Post`.
@@ -17,6 +17,10 @@ def post_detail(request, slug):
 
     ``post``
         An instance of :model:`blog.Post`.
+    ``comments``
+        All comments related to the post, ordered by creation date (descending).
+    ``comment_count``
+        Count of approved comments for the post.
 
     **Template:**
 
@@ -26,8 +30,16 @@ def post_detail(request, slug):
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
 
+    # Retrieve and order comments, count approved comments
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+
     return render(
         request,
-        "blog/post_details.html",
-        {"post": post},
+        "blog/post_detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+        },
     )
